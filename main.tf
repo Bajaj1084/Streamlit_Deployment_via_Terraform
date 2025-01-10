d026170ab084e97058e7b6760047955a9361b415
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-11 00:29:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,60 +5,27 @@ terraform {
       version = "0.25.17"
     }
   }
+
+
+  backend "remote" {
+    organization = "my-organization-name"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
 
-provider "snowflake" {}
-
-# Create Snowflake role
-resource "snowflake_role" "app_role" {
-  name = "STREAMLIT_APP_ROLE"
+provider "snowflake" {
 }
 
-# Create Snowflake database
-resource "snowflake_database" "app_db" {
-  name = "STREAMLIT_APP_DB"
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB_v2"
+  comment = "Database for Snowflake Terraform demo"
 }
 
-# Create Snowflake schema
-resource "snowflake_schema" "app_schema" {
-  name     = "APP_SCHEMA"
-  database = snowflake_database.app_db.name
-}
-
-# Create Snowflake warehouse
-resource "snowflake_warehouse" "app_wh" {
-  name                 = "STREAMLIT_APP_WH"
-  warehouse_size       = "XSMALL"
-  auto_suspend         = 300
-  auto_resume          = true
-  initially_suspended  = false
-}
-
-# Grant usage and privileges on account level
-
-
-# Grant usage on the database
-
-
-# Grant usage on the schema
-
-
-# Grant usage on the warehouse
-
-
-
-
- 
-
-# Outputs for confirmation
-output "warehouse_name" {
-  value = snowflake_warehouse.app_wh.name
-}
-
-output "database_name" {
-  value = snowflake_database.app_db.name
-}
-
-output "schema_name" {
-  value = snowflake_schema.app_schema.name
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA_v2"
+  comment  = "Schema for Snowflake Terraform demo"
 }
