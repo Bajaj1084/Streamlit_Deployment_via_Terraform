b0db09a57227d6429824caa8cfb8d6aa720d7f80
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-10 23:58:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,81 +35,20 @@ resource "snowflake_warehouse" "app_wh" {
 }
 
 # Grant usage and privileges on account level
-resource "snowflake_grant" "account_grants" {
-  for_each = toset(["USAGE"])
-  
-  grants_on {
-    account = true
-  }
 
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
 
 # Grant usage on the database
-resource "snowflake_grant" "database_grants" {
-  for_each = toset(["USAGE", "CREATE SCHEMA", "CREATE TABLE", "SELECT", "INSERT"])
 
-  grants_on {
-    object_name = snowflake_database.app_db.name
-    object_type = "DATABASE"
-  }
-
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
 
 # Grant usage on the schema
-resource "snowflake_grant" "schema_grants" {
-  for_each = toset(["USAGE", "SELECT"])
 
-  grants_on {
-    object_name = "\"${snowflake_database.app_db.name}\".\"${snowflake_schema.app_schema.name}\""
-    object_type = "SCHEMA"
-  }
-
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
 
 # Grant usage on the warehouse
-resource "snowflake_grant" "warehouse_grants" {
-  for_each = toset(["USAGE", "OPERATE", "MONITOR"])
 
-  grants_on {
-    object_name = snowflake_warehouse.app_wh.name
-    object_type = "WAREHOUSE"
-  }
 
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
 
-# Future grants for tables in the database
-resource "snowflake_grant" "future_table_grants_in_database" {
-  grants_on {
-    future_grants_in {
-      database = snowflake_database.app_db.name
-    }
-    object_type = "TABLE"
-  }
 
-  privileges = ["SELECT", "INSERT"]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Future grants for tables in the schema
-resource "snowflake_grant" "future_table_grants_in_schema" {
-  grants_on {
-    future_grants_in {
-      schema = "\"${snowflake_database.app_db.name}\".\"${snowflake_schema.app_schema.name}\""
-    }
-    object_type = "TABLE"
-  }
-
-  privileges = ["SELECT", "INSERT"]
-  roles      = [snowflake_role.app_role.name]
-}
+ 
 
 # Outputs for confirmation
 output "warehouse_name" {
