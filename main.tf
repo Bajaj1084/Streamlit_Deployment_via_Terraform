b18d8fb8e32d8e96289685861bb2f4cab66f06e2
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-11 00:38:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ provider "snowflake" {
 }
 
 resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB_v2"
+  name    = "DEMO_DB_V3"
   comment = "Database for Snowflake Terraform demo"
 }
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
-  name     = "DEMO_SCHEMA_v2"
+  name     = "DEMO_SCHEMA_V3"
   comment  = "Schema for Snowflake Terraform demo"
 }
