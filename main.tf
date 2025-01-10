6e52b8e64ca2b85076c198f271a06e50a2adb19d
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-10 23:08:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,27 +5,32 @@ terraform {
       version = "0.25.17"
     }
   }
+
+  backend "remote" {
+    organization = "my-organization-name"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
 
-provider "snowflake" {}
+provider "snowflake" {
+}
 
-# Create Snowflake role
 resource "snowflake_role" "app_role" {
   name = "STREAMLIT_APP_ROLE"
 }
 
-# Create Snowflake database
 resource "snowflake_database" "app_db" {
   name = "STREAMLIT_APP_DB"
 }
 
-# Create Snowflake schema
 resource "snowflake_schema" "app_schema" {
   name     = "APP_SCHEMA"
   database = snowflake_database.app_db.name
 }
 
-# Create Snowflake warehouse
 resource "snowflake_warehouse" "app_wh" {
   name                 = "STREAMLIT_APP_WH"
   warehouse_size       = "XSMALL"
@@ -34,92 +39,24 @@ resource "snowflake_warehouse" "app_wh" {
   initially_suspended  = false
 }
 
-# Grant usage and privileges on account level
-resource "snowflake_grant" "account_grants" {
-  for_each = toset(["USAGE"])
-  
-  grants_on {
-    account = true
+resource "snowflake_role_grants" "app_role_grants" {
+  role_name = snowflake_role.app_role.name
+
+  database_privileges {
+    database_name = snowflake_database.app_db.name
+    privileges    = ["USAGE", "CREATE SCHEMA"]
   }
 
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Grant usage on the database
-resource "snowflake_grant" "database_grants" {
-  for_each = toset(["USAGE", "CREATE SCHEMA", "CREATE TABLE", "SELECT", "INSERT"])
-
-  grants_on {
-    object_name = snowflake_database.app_db.name
-    object_type = "DATABASE"
+  warehouse_privileges {
+    warehouse_name = snowflake_warehouse.app_wh.name
+    privileges     = ["USAGE"]
   }
-
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
 }
 
-# Grant usage on the schema
-resource "snowflake_grant" "schema_grants" {
-  for_each = toset(["USAGE", "SELECT"])
-
-  grants_on {
-    object_name = "\"${snowflake_database.app_db.name}\".\"${snowflake_schema.app_schema.name}\""
-    object_type = "SCHEMA"
-  }
-
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Grant usage on the warehouse
-resource "snowflake_grant" "warehouse_grants" {
-  for_each = toset(["USAGE", "OPERATE", "MONITOR"])
-
-  grants_on {
-    object_name = snowflake_warehouse.app_wh.name
-    object_type = "WAREHOUSE"
-  }
-
-  privileges = [each.value]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Future grants for tables in the database
-resource "snowflake_grant" "future_table_grants_in_database" {
-  grants_on {
-    future_grants_in {
-      database = snowflake_database.app_db.name
-    }
-    object_type = "TABLE"
-  }
-
-  privileges = ["SELECT", "INSERT"]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Future grants for tables in the schema
-resource "snowflake_grant" "future_table_grants_in_schema" {
-  grants_on {
-    future_grants_in {
-      schema = "\"${snowflake_database.app_db.name}\".\"${snowflake_schema.app_schema.name}\""
-    }
-    object_type = "TABLE"
-  }
-
-  privileges = ["SELECT", "INSERT"]
-  roles      = [snowflake_role.app_role.name]
-}
-
-# Outputs for confirmation
 output "warehouse_name" {
   value = snowflake_warehouse.app_wh.name
 }
 
 output "database_name" {
   value = snowflake_database.app_db.name
-}
-
-output "schema_name" {
-  value = snowflake_schema.app_schema.name
 }
