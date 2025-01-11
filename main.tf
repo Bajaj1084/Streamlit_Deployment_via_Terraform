417a7259fe011e98f53a4f5f396ee272f0ea79ef
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-11 19:47:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,3 +82,117 @@ resource "snowflake_table" "docs_chunks_table" {
 
 
 
+
+
+# Create File Format
+resource "snowflake_file_format" "csv_ff" {
+  name       = "APP_CSV_FF"
+  database   = "DEMO_DB_V3"
+  schema     = "DEMO_SCHEMA_V3"
+  format_type = "CSV"
+}
+
+# Create Stage
+resource "snowflake_stage" "s3_stage" {
+  name        = "S3LOAD"
+  database    = "DEMO_DB_V3"
+  schema      = "DEMO_SCHEMA_V3"
+  url         = "s3://sfquickstarts/tastybytes-cx/app/"
+  file_format = snowflake_file_format.csv_ff.fully_qualified_name
+  comment     = "Quickstarts S3 Stage Connection"
+}
+
+# Create Documents Table
+resource "snowflake_table" "documents" {
+  name      = "DOCUMENTS"
+  database  = "DEMO_DB_V3"
+  schema    = "DEMO_SCHEMA_V3"
+  comment   = "{\"origin\":\"sf_sit-is\", \"name\":\"voc\", \"version\":{\"major\":1, \"minor\":0}, \"attributes\":{\"is_quickstart\":1, \"source\":\"streamlit\", \"vignette\":\"rag_chatbot\"}}"
+  column {
+    name = "RELATIVE_PATH"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "RAW_TEXT"
+    type = "VARCHAR(16777216)"
+  }
+}
+
+# Create Array Table
+resource "snowflake_table" "array_table" {
+  name     = "ARRAY_TABLE"
+  database = "DEMO_DB_V3"
+  schema   = "DEMO_SCHEMA_V3"
+  column {
+    name = "SOURCE"
+    type = "VARCHAR(6)"
+  }
+  column {
+    name = "SOURCE_DESC"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "FULL_TEXT"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "SIZE"
+    type = "NUMBER(18,0)"
+  }
+  column {
+    name = "CHUNK"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "INPUT_TEXT"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "CHUNK_EMBEDDING"
+    type = "ARRAY"
+  }
+}
+
+# Create Vector Store Table
+resource "snowflake_table" "vector_store" {
+  name     = "VECTOR_STORE"
+  database = "DEMO_DB_V3"
+  schema   = "DEMO_SCHEMA_V3"
+  column {
+    name = "SOURCE"
+    type = "VARCHAR(6)"
+  }
+  column {
+    name = "SOURCE_DESC"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "FULL_TEXT"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "SIZE"
+    type = "NUMBER(18,0)"
+  }
+  column {
+    name = "CHUNK"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "INPUT_TEXT"
+    type = "VARCHAR(16777216)"
+  }
+  column {
+    name = "CHUNK_EMBEDDING"
+    type = "VECTOR(FLOAT, 768)"
+  }
+}
+
+# Data Copy Commands (Run these manually or via an external process as Terraform does not handle data loading directly)
+# COPY INTO TASTY_BYTES_CHATBOT.APP.DOCUMENTS FROM @TASTY_BYTES_CHATBOT.APP.S3LOAD/DOCUMENTS/
+# COPY INTO TASTY_BYTES_CHATBOT.APP.ARRAY_TABLE FROM @TASTY_BYTES_CHATBOT.APP.S3LOAD/VECTOR_STORE/
+# INSERT INTO TASTY_BYTES_CHATBOT.APP.VECTOR_STORE (SELECT ...) -- As per your SQL logic
+
+
+
+
