35993018331bd08869d21d54c55946a4aeb7593b
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-11 19:36:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,4 +38,47 @@ resource "snowflake_warehouse" "app_wh" {
   initially_suspended  = false
 }
 
+resource "snowflake_table" "docs_chunks_table" {
+  database = "DEMO_DB_V3"  # Replace with your database name
+  schema   = "DEMO_SCHEMA_V3"    # Replace with your schema name
+  name     = "DOCS_CHUNKS_TABLE"
+
+  column {
+    name = "RELATIVE_PATH"
+    type = "VARCHAR(16777216)"
+    comment = "Relative path to the PDF file"
+  }
+
+  column {
+    name = "SIZE"
+    type = "NUMBER(38,0)"
+    comment = "Size of the PDF"
+  }
+
+  column {
+    name = "FILE_URL"
+    type = "VARCHAR(16777216)"
+    comment = "URL for the PDF"
+  }
+
+  column {
+    name = "SCOPED_FILE_URL"
+    type = "VARCHAR(16777216)"
+    comment = "Scoped URL (you can choose which one to keep depending on your use case)"
+  }
+
+  column {
+    name = "CHUNK"
+    type = "VARCHAR(16777216)"
+    comment = "Piece of text"
+  }
+
+  column {
+    name = "CATEGORY"
+    type = "VARCHAR(16777216)"
+    comment = "Will hold the document category to enable filtering"
+  }
+}
+
+
 
