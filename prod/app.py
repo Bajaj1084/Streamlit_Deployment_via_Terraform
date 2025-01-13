da87fed1f5bf5a93d78ea256fe93c8087e08fd41
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-13 11:45:01 +0530

    Update app.py

--- prod/app.py
+++ prod/app.py
@@ -1,6 +1,7 @@
 # Import python packages
 # Streamlit
 # Novo Nordisk Streamlit app Demo
+import numpy as np
 import pandas as pd
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
